601a49186d4b64725295ce10a6be3b9b3c9e0956
mordanc <iusethisacc4games@gmail.com>
2021-09-06 21:32:15 -0400

    #2 Added youtube thumbnail library, json server

--- src/components/organisms/ThumbnailList.tsx
+++ src/components/organisms/ThumbnailList.tsx
@@ -1,17 +1,37 @@
 import React from "react";
-import Thumbnail from "../molecules/Thumbnail";
+import YouTube from "react-youtube";
+
+export const ThumbnailSection = ({ videoId }: { videoId: string }) => {
+  const opts = {
+    height: "70%",
+    width: "100%",
+  };
+
+  return (
+    <div>
+      <span className="text-black text-xl">
+        Tom Scott - Why Britain uses separate separate separate separate
+      </span>
+      <div className="mt-4">
+        <YouTube videoId={videoId} id={"string"} opts={opts} />
+      </div>
+    </div>
+  );
+};
 
 const ThumbnailList = () => {
   return (
     <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 px-2 py-2">
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
-      <Thumbnail />
+      <div className="px-2 rounded flex flex-col space-y-4 truncate">
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+        <ThumbnailSection videoId={"9uZam0ubq-Y"} />
+      </div>
     </div>
   );
 };
